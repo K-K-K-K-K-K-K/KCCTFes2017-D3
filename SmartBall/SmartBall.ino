7ca8e2f1a09848f5ebc5fac9f9d68c15594cae47
kcct-dk <r115314@g.kobe-kosen.ac.jp>
2017-10-24 17:06:24 +0900

    update

--- SmartBall/SmartBall.ino
+++ SmartBall/SmartBall.ino
@@ -1,26 +1,26 @@
 void setup() {
   pinMode(2, INPUT); // toggle switch
   pinMode(5, OUTPUT); // electromagnet
-  pinMode(7, OUTPUT); // LED
+  //pinMode(7, OUTPUT); // LED
 
   pinMode(LED_BUILTIN, OUTPUT); // debug
 }
 
 void loop() {
-  if (digitalRead(2) == HIGH) {
+  if (digitalRead(2) == LOW) {
     digitalWrite(5, HIGH);
-    digitalWrite(7, HIGH);
-    while (digitalRead(2) == HIGH) { // from debug
+//    digitalWrite(7, HIGH);
+    while (digitalRead(2) == LOW) { // from debug
       digitalWrite(LED_BUILTIN, HIGH);
-      delay(500);
+      delay(100);
       digitalWrite(LED_BUILTIN, LOW);
-      delay(500);
+      delay(800);
     } // to debug
   } else {
     digitalWrite(5, LOW);
     digitalWrite(7, LOW);
     digitalWrite(LED_BUILTIN, HIGH); // debug
-    while (digitalRead(2) == LOW);
+    while (digitalRead(2) == HIGH);
   }
 }
 
