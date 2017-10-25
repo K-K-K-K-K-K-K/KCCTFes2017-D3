e191d4fab9adf5fcb897f6a025ed5af694e55dec
kcct-dk <r115314@g.kobe-kosen.ac.jp>
2017-10-25 16:11:14 +0900

    update

--- SmartBall/SmartBall.ino
+++ SmartBall/SmartBall.ino
@@ -1,30 +1,69 @@
+const int tgrsw        = 0;
+
+const int elemgt1A  = 1;
+const int elemgt1B  = 2;
+const int led1           = 3;
+
+const int elemgt2A  = 4;
+const int elemgt2B  = 5;
+const int led2           = 6;
+
 void setup() {
-  pinMode(2, INPUT); // toggle switch
-  pinMode(5, OUTPUT); // electromagnet
-  //pinMode(7, OUTPUT); // LED
+  pinMode(tgrsw, INPUT);
+
+  pinMode(elemgt1A, OUTPUT);
+  pinMode(elemgt1B, OUTPUT);
+  //pinMode(led1, OUTPUT);
+
+  //pinMode(elemgt2A, OUTPUT);
+  //pinMode(elemgt2B, OUTPUT);
+  //pinMode(led2, OUTPUT);
 
   pinMode(LED_BUILTIN, OUTPUT); // debug
 }
 
 void loop() {
-  if (digitalRead(2) == LOW) {
-    digitalWrite(5, HIGH);
-//    digitalWrite(7, HIGH);
-    while (digitalRead(2) == LOW) { // from debug
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(100);
-      digitalWrite(LED_BUILTIN, LOW);
-      delay(100);
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(100);
-      digitalWrite(LED_BUILTIN, LOW);
-      delay(800);
-    } // to debug
+  if (digitalRead(tgrsw) == LOW) {
+    while (digitalRead(tgrsw) == LOW) {
+      module1(true);
+      module2(false);
+      digitalWrite(LED_BUILTIN, HIGH); // debug
+      delay(3000);
+      module1(false);
+      module1(true);
+      digitalWrite(LED_BUILTIN, LOW); // debug
+      delay(3000);
+    }
   } else {
-    digitalWrite(5, LOW);
-//    digitalWrite(7, LOW);
+    xseq();
     digitalWrite(LED_BUILTIN, HIGH); // debug
-    while (digitalRead(2) == HIGH);
+    while (digitalRead(tgrsw) == HIGH) {
+      //digitalWrite(led1, HIGH);
+      //digitalWrite(led2, LOW);
+      delay(3000);
+      //digitalWrite(led1, LOW);
+      //digitalWrite(led2, HIGH);
+      delay(3000);
+    }
   }
+}
+
+void module1(bool b) {
+      digitalWrite(elemgt1A, b ? HIGH : LOW);
+      digitalWrite(elemgt1B, b ? HIGH : LOW);
+//      digitalWrite(led1, b ? HIGH : LOW);
+}
+
+void module2(bool b) {
+//      digitalWrite(elemgt2A, b ? HIGH : LOW);
+//      digitalWrite(elemgt2B, b ? HIGH : LOW);
+//      digitalWrite(led2, b ? HIGH : LOW);
+}
+
+void xseq() {
+      digitalWrite(elemgt1A, HIGH);
+      digitalWrite(elemgt1B, HIGH);
+//      digitalWrite(elemgt2A, HIGH);
+//      digitalWrite(elemgt2B, HIGH);
 }
 
